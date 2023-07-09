82fe3f50956a6c7280c60c6c4e3f09d11465952e
Maja Frankowska <majafrankowska88@gmail.com>
2023-07-09 17:11:01 +0200

    Create Sum_Positive_nums.java

---
+++ Sum_Positive_nums.java
@@ -0,0 +1,19 @@
+
+public class PositiveSum{
+
+  public static int sum(int[] arr)
+  {
+    int sum = 0;
+    
+    for(int i=0; i<arr.length; i++)
+    {
+      if(arr[i] > 0)
+      {
+        sum += arr[i];
+      }
+    }
+    
+    return sum;
+  }
+
+}
