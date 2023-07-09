72ba17ac82b0060141ec9d3004ad2cf978281266
Maja Frankowska <majafrankowska88@gmail.com>
2023-07-09 17:11:37 +0200

    Create String_Repeat.java

---
+++ String_Repeat.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static String repeatStr(final int repeat, final String string) {
+      
+      String restr = "";
+      
+      for (int i = 0; i < repeat; i++) {
+        
+        restr += string;
+        
+      }
+      
+      return restr;
+    
+    }
+}
