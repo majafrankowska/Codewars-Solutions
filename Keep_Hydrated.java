ec017db62124fa70ab2765349782526eb65058be
Maja Frankowska <majafrankowska88@gmail.com>
2023-07-12 19:56:50 +0200

    Create Keep_Hydrated.java
    8 kyu

---
+++ Keep_Hydrated.java
@@ -0,0 +1,15 @@
+public class KeepHydrated  {
+
+  public int Liters(double time)  {
+    
+    int num;
+    
+    num = (int) Math.floor(time * 0.5);
+    
+    
+    return num;
+     
+  
+  }
+
+}
