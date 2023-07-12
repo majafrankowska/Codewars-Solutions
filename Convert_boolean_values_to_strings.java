8d1911749c09d960aa94155c1a8b43cc28c10ede
Maja Frankowska <majafrankowska88@gmail.com>
2023-07-12 20:35:40 +0200

    Create Convert_boolean_values_to_strings.java
    8 kyu

---
+++ Convert_boolean_values_to_strings.java
@@ -0,0 +1,24 @@
+
+class YesOrNo
+{
+  public static String boolToWord(boolean b)
+  {
+    //TODO
+    
+    if ( b == true) {
+      
+      //System.out.println("Yes");
+      
+      return "Yes";
+    }
+    
+    else {
+      
+      //System.out.println("No");
+      
+      return "No";
+    }
+    
+  }
+  
+}
