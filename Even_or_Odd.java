dffd3d7cbbd3a315afa9365443ba7e9cc60a28a9
Maja Frankowska <majafrankowska88@gmail.com>
2023-07-12 20:32:19 +0200

    Create Even_or_Odd.java
    8 kyu

---
+++ Even_or_Odd.java
@@ -0,0 +1,13 @@
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        //Place code here
+      if ( number % 2 == 0) {
+        return "Even";
+      }
+      else {
+        return "Odd";
+      }
+        
+    }
+}
