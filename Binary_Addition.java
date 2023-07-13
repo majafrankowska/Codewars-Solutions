45027d8d7008e5246de77c374d6995da1a2bd401
Maja Frankowska <majafrankowska88@gmail.com>
2023-07-13 02:11:46 +0200

    Update Binary_Addition.java
    7 kyu

--- Binary_Addition.java
+++ Binary_Addition.java
@@ -3,38 +3,28 @@ public class Kata{
   
   public static String binaryAddition(int a, int b){
     
-    int sum = a+b;
-    String my_binary = "";
-  
-    int n = sum;
+    int n = a + b;
+    int sum = n;
+    String anwser = "";
     int count = 0;
     
-    while (n>0) {
-      
-      n = n/2;
+    while(sum>0) {
+      sum = sum / 2;
       count++;
-      
     }
     
-    n = sum;
+    int[] arr = new int[count];
     
-    int [] arr = new int [count];
-    
-    for (int i = count - 1; i >=0 ; i--){
-      
-      arr[i] = n % 2;
-      
-      n = n/2;
-      
+    for (int i = count - 1; i>=0; i--) {
+    arr[i] = n%2;
+    n = n / 2;
     }
     
-    for (int i = 0; i<arr.length; i++){
-      
-      my_binary += arr[i];
-      
+    for( int i = 0; i < arr.length; i++) {
+      anwser += arr[i];
     }
-    
-    return my_binary;
+   
+    return anwser;
     
   }
 }
