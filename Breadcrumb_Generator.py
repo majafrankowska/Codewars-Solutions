d317576b11d2a791f3fca13f8f7df438ac82edff
Maja Frankowska <majafrankowska88@gmail.com>
2023-08-18 23:34:20 +0200

    Update Breadcrumb_Generator.py
    4 kyu

--- Breadcrumb_Generator.py
+++ Breadcrumb_Generator.py
@@ -1,22 +1,24 @@
 def generate_bc(url, separator):
     if '//' in url:
-        url = ul[url. index('//') + 2:]
+        url = ul[url.index('//') + 2:]
         
     url = url.rstrip('/')
+    
     try:
         for i, c in enumerate(url):
             if c in ['?', '#']:
                 url = url[0:i]
                 break
-        menus = url. split('/')[1:]
+                
+        menus = url.split('/')[1:]
         if menus and 'index.' == menus [-1] [0:6]:
             menus = menus [:-1]
         if not menus:
             return '<span class="active">HOME</span>'
         breadcrumb = '<a href="/">HOME</a>'
         for i, e in enumerate (menus [:-1]):
-            breadcrumb += separator + '<a href="/(}/">(}</a>'.format('/'.join(menus [:i + 1]), get_element_name(e))
-        breadcrumb += separator + '<span class="active">()</span>'. format(get_element_name (menus [-1]))
+            breadcrumb += separator + '<a href="/{}/">{}</a>'.format('/'.join(menus [:i + 1]), get_element_name(e))
+        breadcrumb += separator + '<span class="active">{}</span>'.format(get_element_name(menus[-1]))
         return breadcrumb
     except:
         return url
@@ -25,13 +27,13 @@ ignore_words = ["the", "of", "in", "from", "by", "with", "and", "or", "for", "to
                                                                            
 def get_element_name (element):
     acronyms = element.split('-')
-    for i, c in enumerate (acronyms [-1]):
+    for i, c in enumerate(acronyms [-1]):
         if c == '.':
             acronyms [-1] = acronyms [-1] [:i]
             break
                                                                            
     if len(element) > 30:
-        for i, c in reversed(list (enumerate(acronyms))):
+        for i, c in reversed(list(enumerate(acronyms))):
             if c in ignore_words: 
                 acronyms.pop(i)                              
         return ''.join([s[0].upper() for s in acronyms])                                                                   
@@ -45,12 +47,12 @@ def generate_bc(url, separator):
     url = sub("https?://", "", url.strip("/"))
     url = sub("/index\..+$","", url)
     url = url.split ("/")
-    url[-1] = sub("\.#?].*", "", url[-1])
+    url[-1] = sub("[\.#?].*", "", url[-1])
               
     menu = ["HOME"]
               
     for item in url[1:]:
-        item = sub("-", "", item.upper())
+        item = sub("-", " ", item.upper())
         if len(item) > 30:
             item = "".join([w[0] for w in item.split() if w not in ignorelist])
         menu.append (item)
