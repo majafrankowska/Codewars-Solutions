c66408de7865603e513ff3a5369c03cd2f6793ba
Maja Frankowska <majafrankowska88@gmail.com>
2023-08-18 23:48:28 +0200

    Update Breadcrumb_Generator.py
    4 kyu

--- Breadcrumb_Generator.py
+++ Breadcrumb_Generator.py
@@ -11,12 +11,12 @@ def generate_bc(url, separator):
                 break
                 
         menus = url.split('/')[1:]
-        if menus and 'index.' == menus [-1] [0:6]:
+        if menus and 'index.' == menus [-1][0:6]:
             menus = menus [:-1]
         if not menus:
             return '<span class="active">HOME</span>'
         breadcrumb = '<a href="/">HOME</a>'
-        for i, e in enumerate (menus [:-1]):
+        for i, e in enumerate(menus [:-1]):
             breadcrumb += separator + '<a href="/{}/">{}</a>'.format('/'.join(menus [:i + 1]), get_element_name(e))
         breadcrumb += separator + '<span class="active">{}</span>'.format(get_element_name(menus[-1]))
         return breadcrumb
@@ -29,7 +29,7 @@ def get_element_name (element):
     acronyms = element.split('-')
     for i, c in enumerate(acronyms [-1]):
         if c == '.':
-            acronyms [-1] = acronyms [-1] [:i]
+            acronyms [-1] = acronyms [-1][:i]
             break
                                                                            
     if len(element) > 30:
@@ -55,7 +55,7 @@ def generate_bc(url, separator):
         item = sub("-", " ", item.upper())
         if len(item) > 30:
             item = "".join([w[0] for w in item.split() if w not in ignoreList])
-        menu.append (item)
+        menu.append(item)
 
     path = ["/"]
     for i in range(len(url) - 1):
@@ -64,6 +64,6 @@ def generate_bc(url, separator):
     html = []
     for i in range(len(url) - 1):
         html.append("<a href=\"" + path[i] + "\">" + menu[i] +"</a>")
-        html.append ("<span class=\"active\">" + menu[-1] +"</span>")
+    html.append("<span class=\"active\">" + menu[-1] +"</span>")
                 
-    return separator.join(html)                  
+    return separator.join(html) 
